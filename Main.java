b2a6690f67d9321f0f3f7be8d4c977deb972fef5
JoelRoth <102542553+JoelRoth@users.noreply.github.com>
2022-11-30 16:03:35 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 package StackMin;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 
+		public static void main(String[] args) {
+	    StackMinMain stack = new StackMinMain();
+	    
+	    stack.push(8);    
+	    stack.push(3);
+	    stack.push(2);
+	    stack.push(9);
+	    stack.push(7);
+	    stack.push(4);
+	    
+	    stack.pop();
+		}
 	}
-
-}
